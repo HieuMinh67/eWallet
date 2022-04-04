c0386f17c97647217f4049f3e5cf956b69115d1d
creolew <phamquochuy3005@gmail.com>
2022-04-04 17:37:50 +0700

    [modify] merchant sql

--- app/db/migrations/V202204041300_merchant_baseline.sql
+++ app/db/migrations/V202204041300_merchant_baseline.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS public.merchant
 (
     account_id UUID,
 	id UUID PRIMARY KEY,
-    merchant_url VARCHAR(200),
+    url VARCHAR(200),
     name VARCHAR(100) NOT NULL,
     FOREIGN KEY(account_id) REFERENCES account (id)
 );
