4d2cced28308d29bea018c499ac228a722324b51
creolew <phamquochuy3005@gmail.com>
2022-04-04 16:50:04 +0700

    [modify] merchant sql baseline

--- app/db/migrations/V202204041100_account_baseline.sql
+++ app/db/migrations/V202204041100_account_baseline.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS public.account
     balance NUMERIC(18,2)
 );
 
-INSERT TABLE public.account (id, type, balance) VALUES ('4da39078-57ed-4c9b-b8eb-f3dcb0603313', 'PERSONAL', 1000.55);
-INSERT TABLE public.account (id, type, balance) VALUES ('ff2a130a-c135-4f76-833d-6b9483308e8d', 'MERCHANT', 1050.55);
+INSERT INTO public.account (id, type, balance) VALUES ('4da39078-57ed-4c9b-b8eb-f3dcb0603313', 'PERSONAL', 1000.55);
+INSERT INTO public.account (id, type, balance) VALUES ('ff2a130a-c135-4f76-833d-6b9483308e8d', 'MERCHANT', 1050.55);
--- app/db/migrations/V202204041300_merchant_baseline.sql
+++ app/db/migrations/V202204041300_merchant_baseline.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS public.merchant
 (
-    id UUID primary key,
+    account_id UUID,
+	id UUID primary key,
+    merchantUrl VARCHAR(200)
     name VARCHAR(100) NOT NULL,
-    account_id foreign key (id UUID)
-    references account (id UUID)
+    foreign key (account_id)
+    references account (id)
 );
 
-INSERT TABLE public.merchant (id, name) VALUES ('ff2a130a-c135-4f76-833d-6b9483308e8d', 'Flower Shop');
+INSERT INTO public.merchant (id, name) VALUES ('ff2a130a-c135-4f76-833d-6b9483308e8d', 'Flower Shop');
