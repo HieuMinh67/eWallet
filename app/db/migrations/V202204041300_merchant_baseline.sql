fda9e471cbb301f2bdd0a85e0bd2cd8a96a055ec
creolew <phamquochuy3005@gmail.com>
2022-04-04 17:35:50 +0700

    [modify] merchant sql

--- app/db/migrations/V202204041300_merchant_baseline.sql
+++ app/db/migrations/V202204041300_merchant_baseline.sql
@@ -1,11 +1,10 @@
 CREATE TABLE IF NOT EXISTS public.merchant
 (
     account_id UUID,
-	id UUID primary key,
-    merchantUrl VARCHAR(200),
+	id UUID PRIMARY KEY,
+    merchant_url VARCHAR(200),
     name VARCHAR(100) NOT NULL,
-    foreign key (account_id)
-    references account (id)
+    FOREIGN KEY(account_id) REFERENCES account (id)
 );
 
 INSERT INTO public.merchant (id, name) VALUES ('ff2a130a-c135-4f76-833d-6b9483308e8d', 'Flower Shop');
