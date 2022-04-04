3d09172b0312f630de394cac31a72eec26dacfe7
Hieu Pham <minhhieu060799@gmail.com>
2022-04-04 11:39:56 +0700

    init

---
+++ app/db/migrations/V202204041100_account_baseline.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS public.account
+(
+    id UUID primary key,
+    type VARCHAR NOT NULL,
+    balance NUMERIC(18,2)
+);
+
+INSERT TABLE public.account (id, type, balance) VALUES ('4da39078-57ed-4c9b-b8eb-f3dcb0603313', 'PERSONAL', 1000.55);
+INSERT TABLE public.account (id, type, balance) VALUES ('ff2a130a-c135-4f76-833d-6b9483308e8d', 'MERCHANT', 1050.55);
